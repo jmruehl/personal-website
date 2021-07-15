04d1f6c41914a86cc0476304f9c8512b33d4a33c
Merle <jmruehl@gmail.com>
2021-07-15 16:27:57 +0200

    update layout, adding mist-recent section

--- js/logbook.js
+++ js/logbook.js
@@ -1,8 +1,20 @@
 const btns = document.querySelectorAll('.display-text')
-const text = document.querySelectorAll('.hidden')
+const fullText = document.querySelectorAll('.hidden')
 
+const logEntries = document.querySelectorAll('.log-entry')
+const insertMostRecentEntry = document.querySelector('#insert-text')
+
+
+
+// Display most recent (=last )entry of log book on top of page
+
+insertMostRecentEntry.innerHTML = logEntries[logEntries.length -1].innerHTML
+
+
+
+//Display full text of log book entry
 
 btns.forEach((el, i) => el.addEventListener('click', event => {
     el.classList.add('hidden')
-    text[i].classList.remove('hidden')
+    fullText[i].classList.remove('hidden')
     }))
