4427501535efd2c6c2eba41552a2039962f37dd0
Merle <jmruehl@gmail.com>
2021-07-19 12:54:25 +0200

    Fixed Display Button/ Entry 07-19

--- js/logbook.js
+++ js/logbook.js
@@ -1,18 +1,18 @@
-const btns = document.querySelectorAll('.display-text')
-const fullText = document.querySelectorAll('.hidden')
+
+// Display most recent (=last) entry of log book on top of page
 
 const logEntries = document.querySelectorAll('.log-entry')
 const insertMostRecentEntry = document.querySelector('#insert-text')
-
-
-
-// Display most recent (=last )entry of log book on top of page
 
 insertMostRecentEntry.innerHTML = logEntries[logEntries.length -1].innerHTML
 
 
 
 //Display full text of log book entry
+
+
+const btns = document.querySelectorAll('.display-text')
+const fullText = document.querySelectorAll('.hidden')
 
 btns.forEach((el, i) => el.addEventListener('click', event => {
     el.classList.add('hidden')
